57aa45c3ed38a1e91ddd4d2554310fa05540764a
osman <osmancibik.caliskan@gmail.com>
2021-09-16 22:59:13 +0300

    added

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useState, useReducer } from "react";
+import { reducer } from "./reducer";
+
+const initialState = {
+  data: '',
+  loading: false,
+  error: ''
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const {data, loading, error} = state;
+
+  // const [data, setData] = useState("");
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState("");
+
+  const fetchData = () => {
+    dispatch({type: 'FETCH_START'});
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((res) => {
+        dispatch({type: 'FETCH_SUCCESS', payload: res.message})
+      })
+      .catch(() => {
+        dispatch({type: 'FETCH_FAIL', payload: 'Something went wrong!'})
+      })
+  }
+
+  // const fetchData = () => {
+  //   setLoading(true);
+  //   setError("");
+  //   setData("");
+
+  //   fetch('https://api.thecatapi.com/v1/images/search')
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setLoading(false);
+  //       setData(res[0].url);
+  //     })
+  //     .catch(() => {
+  //       setLoading(false);
+  //       setError("Something went wrong!");
+  //     });
+  // };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <button
+        onClick={fetchData}
+        disabled={loading}
+        style={{ width: "100px", margin: "1rem" }}
+      >
+        Fetch Data
+      </button>
+      {data && <img src={data} alt="cat-img" />}
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
+
+export default App;
